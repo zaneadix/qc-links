daad6869b99a759a55f89d38835dfcb6f56a8ee2
Zane Adickes <zaneadix@gmail.com>
2019-06-16 23:55:26 -0400

    farts

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,33 +9,33 @@ export default props => {
   return (
     <div>
       <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
-      {/* {data.remarks.links.map(({ node: { data } }) => {
+      {data.remarks.links.map(({ node: { data } }) => {
         return (
           <div>
-            <img src={data.imageUrl} alt={data.destinationUrl} />
             <a href={data.destinationUrl} target="_blank_">
-              Go
+              <img src={data.imageUrl} alt={data.title} />
             </a>
           </div>
         )
-      })} */}
+      })}
     </div>
   )
 }
 
-// export const query = graphql`
-//   query {
-//     remarks: allMarkdownRemark(
-//       sort: { order: DESC, fields: [frontmatter___publishTime] }
-//     ) {
-//       links: edges {
-//         node {
-//           data: frontmatter {
-//             imageUrl
-//             destinationUrl
-//           }
-//         }
-//       }
-//     }
-//   }
-// `
+export const query = graphql`
+  query {
+    remarks: allMarkdownRemark(
+      sort: { order: DESC, fields: [frontmatter___publishTime] }
+    ) {
+      links: edges {
+        node {
+          data: frontmatter {
+            title
+            imageUrl
+            destinationUrl
+          }
+        }
+      }
+    }
+  }
+`
