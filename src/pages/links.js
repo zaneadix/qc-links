8afa0c3253c51a1000e67fb09daf644d6e24a117
Zane Adickes <zane@Zanes-MacBook-Pro.local>
2020-05-19 22:41:09 -0400

    tiny tweak

--- src/pages/links.js
+++ src/pages/links.js
@@ -10,28 +10,18 @@ import { mediaQueries } from "../utils/styleVars"
 let linksStyles = css`
   display: flex;
   flex-wrap: wrap;
-  margin-right: -0.5rem;
-  margin-left: -0.5rem;
+  margin-right: -0.75rem;
+  margin-left: -0.75rem;
   padding-top: 1rem;
-
-  ${mediaQueries[2]} {
-    margin-right: -1rem;
-    margin-left: -1rem;
-  }
 
   .link {
     flex: 1 0 50%;
     max-width: 50%;
-    padding: 1rem;
+    padding: 0.75rem;
 
     ${mediaQueries[1]} {
       flex: 1 0 33.3%;
       max-width: 33.3%;
-    }
-
-    ${mediaQueries[2]} {
-      flex: 1 0 25%;
-      max-width: 25%;
     }
   }
 `
